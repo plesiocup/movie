4ed1f5bebffe927a8c083ec4d233647f0517b795
smaru1111 <marusou20@gmail.com>
2023-12-17 01:50:53 +0900

    [modify] 評価とカテゴリ対応するようにした

--- src/functions/getMovieData.ts
+++ src/functions/getMovieData.ts
@@ -27,18 +27,20 @@ export async function getMovieData(
   const movieData: MovieData[] = []
   responseMovieData.results.forEach((responseMovieData: any) => {
     // "original_title": "Blue Beetle",
-    // "id": 565770,
+    // id: 565770,
     // https://www.themoviedb.org/movie/565770-blue-beetle/watch?locale=AE
     //   スネークケースに変換
     let movieTitle = responseMovieData.title
     movieTitle = movieTitle.toLowerCase().replace(/ /g, '-')
     const movieUrl = `https://www.themoviedb.org/movie/${responseMovieData.id}-${responseMovieData.original_title}/watch?locale=AE`
+
+    const category = getGenreNameById(responseMovieData.genre_ids[0])
     movieData.push({
       title: responseMovieData.title,
       description: responseMovieData.overview,
-      category: responseMovieData.original_language,
+      category: category,
       play_time: responseMovieData.popularity,
-      evaluation: responseMovieData.vote_average,
+      evaluation: responseMovieData.vote_average / 2,
       evaluated_count: responseMovieData.vote_count,
       release_year: responseMovieData.release_date,
       movie_url: movieUrl,
@@ -80,4 +82,88 @@ const fetchMovieData = async () => {
       body: 'An error occurred while fetching the movie data.',
     }
   }
+}
+
+const genres = [
+  {
+    id: 28,
+    name: 'Action',
+  },
+  {
+    id: 12,
+    name: 'Adventure',
+  },
+  {
+    id: 16,
+    name: 'Animation',
+  },
+  {
+    id: 35,
+    name: 'Comedy',
+  },
+  {
+    id: 80,
+    name: 'Crime',
+  },
+  {
+    id: 99,
+    name: 'Documentary',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+  },
+  {
+    id: 10751,
+    name: 'Family',
+  },
+  {
+    id: 14,
+    name: 'Fantasy',
+  },
+  {
+    id: 36,
+    name: 'History',
+  },
+  {
+    id: 27,
+    name: 'Horror',
+  },
+  {
+    id: 10402,
+    name: 'Music',
+  },
+  {
+    id: 9648,
+    name: 'Mystery',
+  },
+  {
+    id: 10749,
+    name: 'Romance',
+  },
+  {
+    id: 878,
+    name: 'Science Fiction',
+  },
+  {
+    id: 10770,
+    name: 'TV Movie',
+  },
+  {
+    id: 53,
+    name: 'Thriller',
+  },
+  {
+    id: 10752,
+    name: 'War',
+  },
+  {
+    id: 37,
+    name: 'Western',
+  },
+]
+
+function getGenreNameById(id: number): string | undefined {
+  const genre = genres.find((genre) => genre.id === id)
+  return genre?.name
 }
